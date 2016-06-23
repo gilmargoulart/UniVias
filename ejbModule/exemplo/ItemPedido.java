3ef509b910e93a48b145db371bb791df4e815a5c
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2016-06-23 08:34:33 -0300

    Adicionado exemplo

---
+++ ejbModule/exemplo/ItemPedido.java
@@ -0,0 +1,27 @@
+package exemplo;
+
+public class ItemPedido {
+	
+	private long id;
+	private int quantidade;
+	private float preco;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	
+}
---
+++ ejbModule/exemplo/Pedido.java
@@ -0,0 +1,44 @@
+package exemplo;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private Date data;
+	private float valorTotal;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "pedido_oid")
+	private List<ItemPedido> itens;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public float getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(float valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+}
