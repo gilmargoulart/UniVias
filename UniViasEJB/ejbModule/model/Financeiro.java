cc81a8c9ab41c66149bb4f99dadfc8e90203c588
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2016-08-01 22:01:42 -0300

    Mapeando entidades inquilino financeiro

--- UniViasEJB/ejbModule/model/Financeiro.java
+++ UniViasEJB/ejbModule/model/Financeiro.java
@@ -1,5 +1,131 @@
 package model;
 
-public class Financeiro {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Financeiro {
+	
+	@ManyToOne
+	private Inquilino inquilino;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	/**
+	 * Indica o tipo do documento financeiro:
+	 * 	- A pagar
+	 * 	- A receber
+	 */
+	private short tipo;
+	
+	/**
+	 * Número do documento
+	 */
+	private String nroDocumento;
+	
+	/**
+	 * Valor
+	 */
+	private float valor;
+	
+	/**
+	 * Origem: indica a partir de onde foi gerado o título no financeiro
+	 * Pode ser gerado a partir de:
+	 * 	- 
+	 */
+	private String origem;
+	
+	@Temporal(value = TemporalType.DATE)
+	private Date vencimento;
+	
+	/**
+	 * Valor de juros
+	 */
+	private float juros;
+	
+	/**
+	 * Valor de desconto
+	 */
+	private float desconto;
+
+	public Inquilino getInquilino() {
+		return inquilino;
+	}
+
+	public void setInquilino(Inquilino inquilino) {
+		this.inquilino = inquilino;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public short getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(short tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNroDocumento() {
+		return nroDocumento;
+	}
+
+	public void setNroDocumento(String nroDocumento) {
+		this.nroDocumento = nroDocumento;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public String getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(String origem) {
+		this.origem = origem;
+	}
+
+	public Date getVencimento() {
+		return vencimento;
+	}
+
+	public void setVencimento(Date vencimento) {
+		this.vencimento = vencimento;
+	}
+
+	public float getJuros() {
+		return juros;
+	}
+
+	public void setJuros(float juros) {
+		this.juros = juros;
+	}
+
+	public float getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(float desconto) {
+		this.desconto = desconto;
+	}
 }
--- UniViasEJB/ejbModule/model/Inquilino.java
+++ UniViasEJB/ejbModule/model/Inquilino.java
@@ -11,19 +11,79 @@ public class Inquilino {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
+	/**
+	 * Nome completo
+	 */
 	private String nome;
-	
+
+	/**
+	 * Endereço de e-mail
+	 */
+	private String email;
+
+	/**
+	 * Número de telefone 
+	 */
+	private String telefone;
+
+	/**
+	 * Identificação do responsável.
+	 * Ex: Nome completo
+	 */
+	private String responsavel;
+
+	/**
+	 * Documento de identificação.
+	 * Ex: CNPJ, CPF, RG
+	 */
+	private String doctoIdentificacao;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	public String getDoctoIdentificacao() {
+		return doctoIdentificacao;
+	}
+
+	public void setDoctoIdentificacao(String doctoIdentificacao) {
+		this.doctoIdentificacao = doctoIdentificacao;
+	}
 }
