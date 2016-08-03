c1dc007659f4c86601371d5d1a311dd6a8abc7a8
JaysonCosta <jayson.costa@gmail.com>
2016-08-03 20:20:00 -0300

    mapeado entidades. - contrato - Geolocalização - Localização

--- UniViasEJB/ejbModule/model/Geolocalizacao.java
+++ UniViasEJB/ejbModule/model/Geolocalizacao.java
@@ -1,5 +1,30 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+
+@Embeddable
 public class Geolocalizacao {
+	
+	
+	@Column
+	private float geoX;
+	@Column
+	private float geoY;
+	
+	
+	public float getGeoX() {
+		return geoX;
+	}
+	public void setGeoX(float geoX) {
+		this.geoX = geoX;
+	}
+	public float getGeoY() {
+		return geoY;
+	}
+	public void setGeoY(float geoY) {
+		this.geoY = geoY;
+	}
 
 }
--- UniViasEJB/ejbModule/model/Localizacao.java
+++ UniViasEJB/ejbModule/model/Localizacao.java
@@ -1,5 +1,75 @@
 package model;
 
+import java.util.Date;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Localizacao {
+	
+	@ManyToOne
+	private Inquilino inquilino;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private int idrota;
+	private String tipo;
+	private String cep;
+	@Temporal(value = TemporalType.TIME)
+	private Date horario;
+	@Embedded
+	private Geolocalizacao geolocalizacao;
+	
+	public Inquilino getInquilino() {
+		return inquilino;
+	}
+	public void setInquilino(Inquilino inquilino) {
+		this.inquilino = inquilino;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdrota() {
+		return idrota;
+	}
+	public void setIdrota(int idrota) {
+		this.idrota = idrota;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public Date getHorario() {
+		return horario;
+	}
+	public void setHorario(Date horario) {
+		this.horario = horario;
+	}
+	public Geolocalizacao getGeolocalizacao() {
+		return geolocalizacao;
+	}
+	public void setGeolocalizacao(Geolocalizacao geolocalizacao) {
+		this.geolocalizacao = geolocalizacao;
+	}
 
 }
