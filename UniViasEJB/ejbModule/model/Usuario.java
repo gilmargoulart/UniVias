486247b96bde913543859eddb096aa7380a3b0fc
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2016-09-28 20:42:24 -0300

    Adicionado entidade para usuários

---
+++ UniViasEJB/ejbModule/model/Usuario.java
@@ -0,0 +1,71 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Usuario {
+	
+	@ManyToOne
+	@JoinColumn
+	private Inquilino inquilino;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(length = 120)
+	private String nome;
+	
+	@Column(length = 240)
+	private String login;
+	
+	@Column(length = 128)
+	private String senha;
+
+	public Inquilino getInquilino() {
+		return inquilino;
+	}
+
+	public void setInquilino(Inquilino inquilino) {
+		this.inquilino = inquilino;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
